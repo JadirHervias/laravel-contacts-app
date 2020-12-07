1f28a04b7ba389a22ce5eaf22e2ded7bf65776b8
Jadir Hervias <jadirhervias@gmail.com>
2020-12-06 23:41:55 -0500

    Practica 1 v11

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,23 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Inicio') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                    <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
+                        <div class="jumbotron jumbotron-fluid">
+                        <div class="container">
+                            <h1 class="display-4">PR&Aacute;CTICA 1 - JADIR HERVIAS</h1>
+                            <p class="lead">Desarrollo de soluciones en la nube</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,6 @@
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="jumbotron jumbotron-fluid">
                     <div class="container">
