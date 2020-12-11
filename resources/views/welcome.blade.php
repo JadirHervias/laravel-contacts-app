e580227541309e6647ad1afe093ad2788acb85c6
Jadir Hervias <jadirhervias@gmail.com>
2020-12-10 19:17:22 -0500

    UI update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,10 +35,10 @@
                             </x-jet-dropdown-link>
                         </form>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+                        <a href="{{ route('login') }}" class="text-lg text-gray-700 underline">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-lg text-gray-700 underline">Register</a>
                         @endif
                     @endif
                 </div>
